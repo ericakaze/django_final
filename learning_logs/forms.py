3fa3f0cf7c61d04fd2c420ad097a1dcd3f097724
ericakaze <erica_kaze@baylor.edu>
2020-05-01 19:05:35 -0500

    notes

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -13,7 +13,7 @@ class TopicForm(forms.ModelForm):
 class EntryForm(forms.ModelForm):
     class Meta:
         model = Entry
-        fields = ["text"]
-        labels = {"text": ""}
+        fields = ['text']
+        labels = {'text': 'Entry'}
 
-        widgets = {"text": forms.Textarea(attrs={"cols": 80})}
+        widgets = {'text': forms.Textarea(attrs={'cols': 80})}
